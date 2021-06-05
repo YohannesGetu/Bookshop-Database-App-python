c30755cd25bf73a39e9a134c2da8ad15269e88bb
Yohannes Getu <nede.evoli@gmail.com>
2021-06-05 12:00:15 +0300

    added integration of backend to fronend

--- frontend.py
+++ frontend.py
@@ -1,7 +1,65 @@
 from tkinter import *
 import backend
 
+
+def change_entries(title="", author="", year="", isbn=""):
+    e1.delete(0, END)
+    e1.insert(END, title)
+    e2.delete(0, END)
+    e2.insert(END, author)
+    e3.delete(0, END)
+    e3.insert(END, year)
+    e4.delete(0, END)
+    e4.insert(END, isbn)
+
+
+def get_selected_row(event):
+    global selected_row
+    if len(list1.curselection()) > 0:
+        index = list1.curselection()[0]
+        selected_row = list1.get(index)
+        change_entries(selected_row[1], selected_row[2],
+                       selected_row[3], selected_row[4])
+    
+
+
+def view_command():
+    list1.delete(0, END)
+    for row in backend.view():
+        list1.insert(END, row)
+
+
+def search_command():
+    list1.delete(0, END)
+    for row in backend.search(title=title_text.get(), author=author_text.get(), year=year_text.get(), isbn=isbn_text.get()):
+        list1.insert(END, row)
+
+
+def add_command():
+    backend.insert(title=title_text.get(), author=author_text.get(),
+                   year=year_text.get(), isbn=isbn_text.get())
+    list1.delete(0, END)
+    list1.insert(END, (title_text.get(), author_text.get(),
+                 year_text.get(), isbn_text.get()))
+    change_entries()
+
+
+def update_command():
+    backend.update(id=selected_row[0], title=title_text.get(), author=author_text.get(),
+                   year=year_text.get(), isbn=isbn_text.get())
+    change_entries()
+    view_command()
+
+
+def delete_command():
+    backend.delete(selected_row[0])
+    change_entries()
+    view_command()
+
+
 window = Tk()
+
+window.wm_title("BookStore")
 
 l1 = Label(window, text="Title")
 l1.grid(row=0, column=0)
@@ -40,22 +98,24 @@ sb1.grid(row=2, column=2, rowspan=7)
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
-b1 = Button(window, text="View all", width=12)
+list1.bind('<<ListboxSelect>>', get_selected_row)
+
+b1 = Button(window, text="View all", width=12, command=view_command)
 b1.grid(row=2, column=3)
 
-b2 = Button(window, text="Search entry", width=12)
+b2 = Button(window, text="Search entry", width=12, command=search_command)
 b2.grid(row=3, column=3)
 
-b3 = Button(window, text="Add entry", width=12)
+b3 = Button(window, text="Add entry", width=12, command=add_command)
 b3.grid(row=4, column=3)
 
-b4 = Button(window, text="Update", width=12)
+b4 = Button(window, text="Update selected", width=12, command=update_command)
 b4.grid(row=5, column=3)
 
-b5 = Button(window, text="Delete", width=12)
+b5 = Button(window, text="Delete selected", width=12, command=delete_command)
 b5.grid(row=6, column=3)
 
-b6 = Button(window, text="Close", width=12)
+b6 = Button(window, text="Close", width=12, command=window.destroy)
 b6.grid(row=7, column=3)
 
 window.mainloop()
